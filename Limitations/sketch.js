fb77a78899f529f4f19ef8b5f3ee12915f12ca48
vandalin10 <42970885+vandalin10@users.noreply.github.com>
2018-12-12 19:20:41 -0500

    Add files via upload

--- Limitations/sketch.js
+++ Limitations/sketch.js
@@ -12,55 +12,61 @@ var pillsY = 180;
 var penX = 440;
 var penY = 450;
 
-var lightDrizzle;
+var Drizzle;
+var Sobbing;
+
 var raindrops = [];
 
 var meeting;
 var crying;
+var crying2;
 var pills;
 var kidTV;
 var murTV;
 var statue;
+var statue2;
 var pen;
 var police;
 var handcuffed;
 
 
-var crying2;
-var beforedead = false;
-
 
 
 function preload(){
-	//lightDrizzle = loadSound("assets/LightDrizzle.wav");
+	Drizzle = loadSound("assets/Drizzle.wav");
+	Sobbing = loadSound("assets/Sobbing.wav");
+
+
 	meeting = loadImage("assets/meeting.png"); //scene0
 	crying = loadImage("assets/crying.png"); //scene 1
+	crying2 = loadImage("assets/crying2.png");
 	pills = loadImage("assets/pills.png");
 	kidTV = loadImage("assets/kidnapped.png"); //scene2
 	murTV = loadImage("assets/murder.png"); //scene4
 	statue = loadImage("assets/statue.png"); //scene5
+	statue2 = loadImage("assets/statue2.png");
 	pen = loadImage("assets/pen.png");
-	police = loadImage("assets/police.jpg"); //scene6
-	handcuffed = loadImage("assets/handcuffed.png") //scene7
+	police = loadImage("assets/police.png"); //scene6
+	handcuffed = loadImage("assets/handcuffed.jpg") //scene7
 
 	nextButton = loadImage("assets/next.png");
 	replayButton = loadImage("assets/replay.png");
-
-	crying2 = loadImage("assets/crying2.png");
 
 }
 
 function setup() {
 
 	createCanvas(1000,800);
+	Drizzle.play();
   // put setup code here
 }
 
-
+var beforedead = false;
 function draw() {
 
 	console.log(mouseX, mouseY);
 	background(169);
+	text(stateIndex,50,50)
 	switch(stateIndex){
 		case 0:
 			outsideScene();
@@ -89,196 +95,233 @@ function draw() {
 
 		default:
 			break;
+	}
+}
 
+function outsideScene(){
+
+	
+	let f = frameCount/120; //creating the animation 
+	for (var i=0; i<random(5); i++) { //quantity of the drizzles
+		raindrops.push(new rain());
+	}
+	for (let drops of raindrops) {
+		drops.update(f); 
+		drops.display(); 
 	}
 
-	function outsideScene(){
 
-		//lightDrizzle.play();
-		let f = frameCount/120; //creating the animation 
-		for (var i=0; i<random(5); i++) { //quantity of the drizzles
-			raindrops.push(new rain());
+	image(meeting,0,0,width,height);
+	image(nextButton,backPosX,backPosY,backSize,backSize);
+
+	if(mouseX>730 && mouseX<880 && mouseY>300 && mouseY<650){ //girl
+		fill(0);
+		textSize(20);
+		text("Sophie L, attending Blank Elementary",600,300);
 		}
-		for (let drops of raindrops) {
-			drops.update(f); 
-			drops.display(); 
-		}
+}
 
-		image(meeting,0,0,width,height);
-		image(nextButton,backPosX,backPosY,backSize,backSize);
+function killingScene(){
 
-		if(mouseX>730 && mouseX<880 && mouseY>300 && mouseY<650){ //girl
+	Drizzle.stop();
+
+	// text(beforedead,50,50)
+
+	if(beforedead == false){
+		image(crying,0,150,1000,700);
+		image(pills,pillsX,pillsY,75,50);
+
+		if(mouseX>265 && mouseX<325 && mouseY>180 && mouseY<220){ //pills
 			fill(0);
 			textSize(20);
-			text("Sophie L, attending Blank Elementary",600,300);
-			}
-	}
-
-	function killingScene(){
-
-		if(beforedead == true){
-			image(crying2,0,150,1000,700);
+			text("Sleeping pills",230,150);
 		}
-		if(beforedead == false){
-			image(crying,0,150,1000,700);
-			image(pills,pillsX,pillsY,75,50);
-			image(nextButton,backPosX,backPosY,backSize,backSize);
 
 		if(mouseX>800 && mouseX<980 && mouseY>350 && mouseY<670){ //crying girl
 			fill(0);
 			textSize(20);
 			text("So annoying, silence her!",750,320);
-			}
-		if(mouseX>265 && mouseX<325 && mouseY>180 && mouseY<220){ //pills
-			fill(0);
-			textSize(20);
-			text("Sleeping pills",230,150);
-			}
 		}
 
-		//if pills are on top of crying girl, img crying2 will appear
-
 	}
+	// console.log("heyy")
 
-	function deadScene(){
+	if(beforedead == true){
+		// console.log(beforedead);
 
-		image(kidTV,0,0,width,height);
+		image(crying2,0,150,1000,700);
 		image(nextButton,backPosX,backPosY,backSize,backSize);
+		Sobbing.stop();
 
+		if(mouseX>860 && mouseX<980 && mouseY>400 && mouseY<670){ //crying girl
+			fill(0);
+			textSize(20);
+			text("...is she dead?",850,380);
+		}
 	}
+}
+//if pills are on top of crying girl, img crying2 will appear
 
-	function timeScene(){
 
-		textSize(50);
-		fill(0);
-		textAlign(CENTER,CENTER);
-		text("25 years later...", width/2, height/2);
-		image(nextButton,backPosX,backPosY,backSize,backSize);
-	}
+function deadScene(){
+	// background(0)
+	image(kidTV,0,0,width,height);
+	image(nextButton,backPosX,backPosY,backSize,backSize);
 
-	function murdererScene(){
+}
 
-		image(murTV,0,0,width,height);
-		image(nextButton,backPosX,backPosY,backSize,backSize);
-	}
+function timeScene(){
 
-	function almostScene(){
+	textSize(50);
+	fill(0);
+	textAlign(CENTER,CENTER);
+	text("25 years later...", width/2, height/2);
+	image(nextButton,backPosX,backPosY,backSize,backSize);
+}
 
+function murdererScene(){
+
+	image(murTV,0,0,width,height);
+	image(nextButton,backPosX,backPosY,backSize,backSize);
+}
+
+function almostScene(){
+
+
+	if(beforedead == false){
 		image(statue,0,0,width,height);
 		image(pen,penX,penY,100,100);
-		image(nextButton,backPosX,backPosY,backSize,backSize);
 
 		if(mouseX>300 && mouseX<700 && mouseY>0 && mouseY<335){ //calender
 			fill(0);
 			textSize(20);
 			text("Mark the day you are free again!",500,40);
-			}
+		}
 		if(mouseX>440 && mouseX<540 && mouseY>460 && mouseY<540){ //pen
 			fill(0);
 			textSize(20);
 			text("Pen",440,500);
-			}
+		}
 	}
 
-	function accuseScene(){
-
-		image(police,0,0,width,height);
+	if(beforedead == true){
+		image(statue2,0,0,width,height);
 		image(nextButton,backPosX,backPosY,backSize,backSize);
+	}
+}
 
-		if(mouseX>180 && mouseX<800 && mouseY>100 && mouseY<600){ //arrest
-			fill(0);
-			textSize(20);
-			text("You are under arrest for the murder or Sophie L",550,600);
-			}
+function accuseScene(){
+
+	image(police,0,0,width,height);
+	image(nextButton,backPosX,backPosY,backSize,backSize);
+
+	if(mouseX>180 && mouseX<800 && mouseY>100 && mouseY<600){ //arrest
+		fill(0);
+		textSize(28);
+		text("You are under arrest for the murder of Sophie L",550,700);
+		}
+}
+
+function caughtScene(){
+
+	image(handcuffed,-100,0,1200,800);
+	image(replayButton,backPosX,backPosY,backSize,backSize);
+
+	if(mouseX>180 && mouseX<800 && mouseY>100 && mouseY<600){ //arrest
+		fill(0);
+		textSize(28);
+		text("You have the right to remain silent",550,650);
+		text("Anything you say can and will be used against you", 550,675);
+		}
+}
+
+
+
+ //end of draw function
+var sceneJustChanged = false
+function mousePressed(){
+
+	if(stateIndex == 0){ //standing in the rain
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 1;
+			sceneJustChanged=true
+			Sobbing.play();
+		}
 	}
 
-	function caughtScene(){
+	else if(stateIndex == 1){ //captured kid crying
 
-		image(handcuffed,0,0,width,height);
-		image(replayButton,backPosX,backPosY,backSize,backSize);
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 2;
+		}
+	}
 
-		if(mouseX>180 && mouseX<800 && mouseY>100 && mouseY<600){ //arrest
-			fill(0);
-			textSize(20);
-			text("You have the right to remain silent",550,600);
-			text("Anything you say can and will be used against you", 550,620);
-			}
+	else if(stateIndex == 2){
+
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 3;
+		}
+	}
+
+	else if(stateIndex == 3){
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 4;
+		}
+	}
+
+	else if(stateIndex == 4){
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 5;
+			beforedead = false
+			sceneJustChanged = true;
+		}
+	}
+
+	else if(stateIndex == 5){
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 6;
+		}
+	}
+
+	else if(stateIndex == 6){
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 7;
+		}
+	}
+
+	else if(stateIndex == 7){
+		if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
+			stateIndex = 0;
+		}
 	}
 
 
+} //end of mouseClicked function 
 
-} //end of draw function
+function mouseDragged(){
+	if(stateIndex == 1 && !sceneJustChanged){
+		pillsX = mouseX
+		pillsY = mouseY;
+	}
 
-	function mousePressed(){
+	if(stateIndex == 5 && !sceneJustChanged){
+		penX = mouseX;
+		penY = mouseY;
+	}
 
-		if(stateIndex == 0){ //standing in the rain
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 1;
-			}
-		}
+}//end of mouseDragged function
 
-		else if(stateIndex == 1){ //captured kid crying
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 2;
-			}
-		}
+function mouseReleased(){
 
-		else if(stateIndex == 2){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 3;
-			}
-		}
-
-		else if(stateIndex == 3){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 4;
-			}
-		}
-
-		else if(stateIndex == 4){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 5;
-			}
-		}
-
-		else if(stateIndex == 5){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 6;
-			}
-		}
-
-		else if(stateIndex == 6){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 7;
-			}
-		}
-
-		else if(stateIndex == 7){
-			if(mouseX>935 && mouseX<970 && mouseY>730 && mouseY<770){
-				stateIndex = 0;
-			}
-		}
-
-
-	} //end of mouseClicked function 
-
-	function mouseDragged(){
-
-		if(stateIndex == 1){
-			pillsX = mouseX
-			pillsY = mouseY;
-		}
-
-		if(stateIndex == 5){
-			penX = mouseX;
-			penY = mouseY;
-		}
-
-	}//end of mouseDragged function
-
-	function mouseReleased(){
+	if (sceneJustChanged){
+		sceneJustChanged = false
+	}
+	else{
 		beforedead = true;
 	}
+
+	//console.log(beforedead);
+}
 
 function rain(){
 
@@ -305,3 +348,4 @@ function rain(){
   }
 
 };
+
